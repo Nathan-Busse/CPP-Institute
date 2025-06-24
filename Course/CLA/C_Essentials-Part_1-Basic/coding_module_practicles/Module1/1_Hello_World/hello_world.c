b5e7d9b034a59f09ef4a2fa78648818c9af19105
Nathan-Busse <bussenathan@icloud.com>
2025-06-24 13:59:49 +0200

    update

---
+++ Course/CLA/C_Essentials-Part_1-Basic/coding_module_practicles/Module1/1_Hello_World/hello_world.c
@@ -0,0 +1,17 @@
+/*
+
+Your first program
+
+Now we’d like to show you a very simple (and completely useless) program written in the “C” language. Using this example, we’ll show you some basic rules as we modify the program many times, enriching it with various elements and simultaneously expanding our programming knowledge.
+
+First, we define our expectations for the program. They’ll be very modest. We want a short and rather meaningless text to appear on the screen. Let's assume that the text should proclaim to the world:
+
+Hello, World!
+*/
+
+#include <stdio.h>
+
+int main(void){
+    puts("Hello, World!");
+    return 0;
+}
