22503cd400027f0e9a1d867576831bcafc2cdc92
Nathan-Busse <bussenathan@icloud.com>
2025-07-08 14:24:48 +0200

    Unary operators

---
+++ course/CLA/C_Essentials-Part_1-Basic/coding_module_practicles/Module_2/Theory/3_Operators/3-unary-operators.c
@@ -0,0 +1,32 @@
+/*
+
+Unary operators in C++ are operators that operate on a single operand. 
+They perform operations such as incrementing or decrementing a value, negating a value, or performing logical negation. 
+
+Here are some common unary operators in C++:
+
+    1. Increment Operator (++) - Increases the value of a variable by 1.
+
+    2. Decrement Operator (--) - Decreases the value of a variable by 1.
+
+    3. Unary Plus (+) - Indicates a positive value (usually redundant).
+
+    4. Unary Minus (-) - Negates the value of a variable.
+
+    5. Logical NOT Operator (!) - Inverts the truth value of a boolean expression.
+
+    6. Bitwise NOT Operator (~) - Inverts the bits of an integer value.
+
+    7. Address-of Operator (&) - Returns the memory address of a variable.
+
+    8. Dereference Operator (*) - Accesses the value at a given memory address.
+
+    9. Sizeof Operator (sizeof) - Returns the size of a data type or variable in bytes.
+
+    10. Type Cast Operators (static_cast, dynamic_cast, const_cast, reinterpret_cast)
+
+*/
+
+
+// These operators are used to manipulate data and perform operations on single operands in C++. 
+// They are essential for various programming tasks, including arithmetic operations, logical operations, and memory management.
